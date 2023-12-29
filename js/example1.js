b39968af13fafa3d678d22724730e064a7ce0b1f
Daiki Arai <darai0512@yahoo.co.jp>
2023-12-29 16:06:51 +0900

    debug

--- js/example1.js
+++ js/example1.js
@@ -1,3 +1,4 @@
+var card1
 (function() {
   'use strict';
 
@@ -13,15 +14,15 @@
     locale: window.__exampleLocale
   });
 
-  var card = elements.create('card', {
+  card1 = elements.create('card', {
     iconStyle: 'solid',
     style: {
       base: {
+        lineHeight: 4,
         iconColor: '#c4f0ff',
         color: '#fff',
         fontWeight: 500,
         fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
-        fontSize: '16px',
         fontSmoothing: 'antialiased',
 
         ':-webkit-autofill': {
@@ -37,7 +38,36 @@
       },
     },
   });
-  card.mount('#example1-card');
+  card1.mount('#example1-card');
 
-  registerElements([card], 'example1');
+  card1._frame._iframe.addEventListener('load', () => {
+    console.log('load')
+    console.log(card1._implementation._frame._iframe.style.height)
+  })
+  let i = true
+  card1.on('ready', () => {
+    console.log('ready')
+    console.log(card1._implementation._frame._iframe.src)
+    console.log(card1._implementation._frame._iframe.style.height)
+    if (i) {
+      setTimeout(() => {
+        i = false
+        var param = {
+          style: {
+            base: {
+              lineHeight: '20px',
+              fontSize: '14px',
+            }
+          }
+        }
+        // card1._implementation._updateFrameHeight(param)
+        card1.update(param)
+        console.log(card1._implementation._frame._iframe.src)
+        console.log(card1._implementation._frame._iframe.style.height)
+      }, 3000)
+    }
+  })
+
+  registerElements([card1], 'example1');
 })();
+
